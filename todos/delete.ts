6dc6e817bd48f66f0d484b747eb53adee637d4fd
Mincho Milev <mincho.milev@gmail.com>
2021-06-27 12:46:41 +0300

    set up local dynamo

---
+++ todos/delete.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+'use strict'
+
+import * as uuid from 'uuid'
+
+import { DynamoDB } from 'aws-sdk'
+
+const dynamoDb = new DynamoDB.DocumentClient()
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  // delete the todo from the database
+  dynamoDb.delete(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t remove the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({}),
+    };
+    callback(null, response);
+  });
+};
---
+++ todos/dynamodb.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import { DynamoDB } from 'aws-sdk'
+
+let options = {};
+
+// connect to local DB if running offline
+if (process.env.IS_OFFLINE) {
+  options = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  };
+}
+
+const client = new DynamoDB.DocumentClient(options);
+
+module.exports = client;
